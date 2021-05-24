e30e079943455b46050e33c6d2566af0efc301e8
ipsitacn <ipsita.mallick@capitalnumbers.com>
2021-05-24 18:52:20 +0530

    bootstrap styling added

--- src/component/Form.tsx
+++ src/component/Form.tsx
@@ -2,44 +2,48 @@ import "./Form.css";
 
 const FormComp: React.FC = () => {
   return (
-    <div className="cotainer">
-      <button className="add-contact">New Contact</button>
+    <div className="container">
+      <button className="new-contact btn btn-primary">New Contact</button>
       <h4>Add new contact</h4>
       <form>
-        <div className="form-group">
-          <input
-            type="text"
-            name="firstname"
-            className="form-control"
-            placeholder="Firstname"
-          />
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <input
+              type="text"
+              name="firstname"
+              className="form-control"
+              placeholder="Firstname"
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <input
+              type="text"
+              name="lastname"
+              className="form-control"
+              placeholder="Lastname"
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <input
+              type="text"
+              name="number"
+              className="form-control"
+              placeholder="Contact Number"
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              className="form-control"
+            />
+          </div>
+          <button className="add-contact btn btn-primary">Add Contact</button>
         </div>
-        <div className="form-group">
-          <input
-            type="text"
-            name="lastname"
-            className="form-control"
-            placeholder="Lastname˜"
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            name="number"
-            className="form-control"
-            placeholder="Contact Number"
-          />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            className="form-control"
-          />
-        </div>
-        <button className="add-contact">Add Contact</button>
+        
       </form>
+      
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import FormComp from "./component/Form";
 
 const App = () => {
   return (
-      <div>
-        Hello
-      </div>
+    <div>
+      <FormComp />
+    </div>
   );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
